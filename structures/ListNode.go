cfc1c474edb0166e0954123105e5c5dbfe94a718
Haoming Wang <haoming.exe@gmail.com>
2020-11-15 21:16:59 +0100

    ListNode structure

---
+++ structures/ListNode.go
@@ -0,0 +1,36 @@
+package structures
+
+import "fmt"
+
+// ListNode is a node in a singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// Array2Node converts an array to a LinkedList
+func Array2Node(nums []int) *ListNode {
+	dummy := &ListNode{}
+	current := dummy
+	for _, num := range nums {
+		current.Next = &ListNode{Val: num}
+		current = current.Next
+	}
+	return dummy.Next
+}
+
+// Node2Array converts a LinkedList to an array
+func Node2Array(head *ListNode) []int {
+	res := []int{}
+	depth, limit := 0, 10
+	for head != nil {
+		depth++
+		if depth > limit {
+			msg := fmt.Sprintf("Current depth %d exceeds limit %d", depth, limit)
+			panic(msg)
+		}
+		res = append(res, head.Val)
+		head = head.Next
+	}
+	return res
+}
---
+++ structures/ListNode_test.go
@@ -0,0 +1,88 @@
+package structures
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestArray2Node(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		args     args
+		expected *ListNode
+	}{
+		{
+			args: args{
+				nums: []int{},
+			},
+			expected: nil,
+		},
+		{
+			args: args{
+				nums: []int{1, 2, 3},
+			},
+			expected: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 3,
+					},
+				},
+			},
+		},
+	}
+	equal := func(a *ListNode, b *ListNode) bool {
+		for a != nil && b != nil {
+			if a.Val != b.Val {
+				return false
+			}
+			a = a.Next
+			b = b.Next
+		}
+		return a == nil && b == nil
+	}
+	for _, tc := range tests {
+		testname := fmt.Sprintf("%v", tc.args.nums)
+		t.Run(testname, func(t *testing.T) {
+			assert.True(t, equal(tc.expected, Array2Node(tc.args.nums)))
+		})
+	}
+}
+
+func TestNode2Array(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		args     args
+		expected []int
+	}{
+		{
+			args: args{
+				head: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+						},
+					},
+				},
+			},
+			expected: []int{1, 2, 3},
+		},
+	}
+	for _, tc := range tests {
+		testname := fmt.Sprintf("%v", tc.expected)
+		t.Run(testname, func(t *testing.T) {
+			assert.Equal(t, tc.expected, Node2Array(tc.args.head))
+		})
+	}
+	tooLong := Array2Node(make([]int, 11))
+	assert.Panics(t, func() { Node2Array(tooLong) }, "Current depth 11 exceeds limit 10")
+}
